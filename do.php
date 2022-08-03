efae5bda32cc9e8d72c2fe96a3ca460b3c70cbfb
acronsgrow <108342058+hasskod4rking777@users.noreply.github.com>
2022-08-04 01:18:32 +0200

    Update and rename do.html to do.php

---
+++ do.php
@@ -0,0 +1,127 @@
+
+
+Skip to content
+Pull requests
+Issues
+Marketplace
+Explore
+@hasskod4rking777
+orangetw /
+My-CTF-Web-Challenges
+Public
+
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+
+    Insights
+
+My-CTF-Web-Challenges/hitcon-ctf-2014/pushincat/do.php /
+@orangetw
+orangetw Initial commit
+Latest commit a815c2b on Aug 1, 2016
+History
+1 contributor
+82 lines (58 sloc) 1.81 KB
+<?php
+
+session_start();
+include "config.php";
+
+$action = $_POST['action'];
+if ($action == 'logout'){
+	session_destroy();
+
+}else if ($action == 'login'){
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	if ( $username == '' or $password == '' ){
+		die( 'parameter error' );
+	}
+
+	$username = pg_escape_string($username);
+	$password = pg_escape_string($password);
+	$sql = "SELECT * FROM users WHERE username='$username' and password='$password'";
+	$res = pg_query($sql);
+	
+	if ( pg_num_rows($res) > 0 ){
+			
+		$result = pg_fetch_object($res);
+		$_SESSION['role'] = $result->role;
+		$_SESSION['ip']   = $result->ip;
+		$_SESSION['username'] = $result->username;
+		$_SESSION['password'] = $result->password;
+
+		die( 'login ok' );
+	} else {
+		die( 'login failed' );
+	}
+
+
+} else if ($action == 'register'){
+
+	$role = 'user';
+	$ip   = $_SERVER['REMOTE_ADDR'];
+	$username = $_POST['username'];
+	$password = $_POST['password'];	
+
+	if ( $username == '' or $password == '' ){
+		die( 'parameter error' );
+	}
+
+	$username = pg_escape_string($username);
+	$sql = "SELECT * FROM users WHERE username='$username'";
+	$res = pg_query($sql);
+	
+	if ( pg_num_rows($res) != 0 ){
+		die( 'registed :(' );
+	}
+
+	$sql = "INSERT INTO users(role, username, password, ip) VALUES('user', '%s', '%s', '%s')";
+	$sql = sprintf($sql, $username, $password, $ip);
+	// $res = pg_query($sql) or die( pg_last_error() );
+	$res = pg_query($sql) or die( error($sql) );
+
+	die( 'register ok' );
+
+} else if ($action == 'flag'){
+
+	$username = $_SESSION['username'];
+	$role = $_SESSION['role'];
+	$ip   = $_SESSION['ip'];
+
+	if ( !isset($username) ){
+		die( 'not login' );
+	}
+
+	if ($role != 'admin'){
+		die( 'You are not admin <br> from ' . $ip );
+	} 
+
+	die( 'fake flag here, try another way :P' );
+
+}
+
+?>
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
